4e7f2f86da125a6a49e3a33903ad489ea628d3b9
Kota Yatagai <kota@yatagai.com>
2026-08-28 00:53:18 +0900

    Generate project files

---
+++ Source/PCS/PCS.Build.cs
@@ -0,0 +1,17 @@
+using UnrealBuildTool;
+
+public class PCS : ModuleRules
+{
+	public PCS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine"
+			});
+	}
+}
